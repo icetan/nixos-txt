061fefa0223b82bcafe484fc57498fa2e9131081
Christopher Fredén <me@icetan.org>
2024-04-12 13:32:39 +0200

    Add flake output nixosModules

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "NixOS module for publising text files";
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosModule = import ./.;
+  outputs = { self, ... }: {
+    nixosModules = {
+      txt = import ./.;
+      default = self.outputs.nixosModules.txt;
+    };
+    nixosModule = self.outputs.nixosModules.txt;
   };
 }
